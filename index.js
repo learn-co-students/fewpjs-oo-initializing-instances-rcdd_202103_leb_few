e3c1cfe5063f738bae390518de0b0762b478bb17
Abdallah Safar <abdallah.safar01@gmail.com>
2021-05-07 15:45:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your code here
+class Breakfast {
+  constructor(food,drink) {
+    this.food = food;
+    this.drink = drink;
+  }
+}
+
+class Lunch {
+  constructor(salad,soup,drink) {
+    this.soup = soup;
+    this.drink = drink;
+    this.salad = salad;
+  }
+}
+
+class Dinner {
+  constructor(salad,soup,entree,dessert) {
+    this.soup = soup;
+    this.entree = entree;
+    this.salad = salad;
+    this._dessert = dessert;
+  }
+}
